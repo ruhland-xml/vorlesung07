bca8e6ed12717c33f11ea052f460125696602e04
hszg-ruhland <kruhland@hszg.de>
2022-05-25 10:23:20 +0200

    keywords

---
+++ keywords.js
@@ -0,0 +1,33 @@
+// Extrahiert alle Keywords aus einem JSON-Objekt obj
+// Ergebnis ist ein Array keywords
+
+const fs = require('fs');
+
+function extractKeywordsFromJson(obj, keywords) {
+    for(var key in obj) {
+        if(obj[key] instanceof Object) {
+            extractKeywordsFromJson(obj[key],keywords);
+        } else {
+            let value = obj[key];
+            if ( typeof value ===  "string" ){
+                newString = value.replace(/[`~!@#$%^&*()_|+\-=?;:,.'"<>\{\}\[\]\\\/]/gi, ' ')
+                let stringArray = newString.split(/(\s+)/).filter( e => e.trim().length > 0);
+                for ( let i=0; i< stringArray.length; i++ ){
+                    let singleString = stringArray[i].toLowerCase();
+                    if ( singleString.length>0 && keywords.includes(singleString) === false )
+                        keywords.push(singleString);
+                }
+            }
+        };
+    }
+};
+
+module.exports = {
+    getKeywords: function(obj) { 
+        let keywords = [];
+        extractKeywordsFromJson(obj,keywords);
+        return keywords;
+    }
+}
+
+
